7ca0d7d1dc20a7f7f31486f045f4251e86329251
angelmark <mark.hsieh@angelsctek.com>
2020-07-07 18:07:44 +0800

    layout

--- idlf/lib/main.dart
+++ idlf/lib/main.dart
@@ -7,12 +7,23 @@ void main() {
 class MarkApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    double size = 77;
+
     return new MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text("Hello"),),
           body: Center(
-            child: Text("W\nO\nR\nL\nD"),))
+            child: Column(
+              children: <Widget>[
+                Text("W", style: TextStyle(fontSize: size)),
+                Text("O", style: TextStyle(fontSize: size)),
+                Text("R", style: TextStyle(fontSize: size)),
+                Text("L", style: TextStyle(fontSize: size)),
+                Text("D", style: TextStyle(fontSize: size)),],
+              mainAxisAlignment: MainAxisAlignment.center,
+            )))
     );
   }
 }

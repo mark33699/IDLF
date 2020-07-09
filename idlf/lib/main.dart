186fb5d7aae85b282c88a63648c6e2e1071a4b38
angelmark <mark.hsieh@angelsctek.com>
2020-07-09 12:03:24 +0800

    margin, padding, stretch, FlutterLogo

--- idlf/lib/main.dart
+++ idlf/lib/main.dart
@@ -8,22 +8,30 @@ class MarkApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    double size = 77;
+    double size = 50;
 
     return new MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text("Hello"),),
-          body: Center(
+          body: Container(
+            color: Colors.amber,
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(40),
+            padding: EdgeInsets.all(30),
             child: Column(
               children: <Widget>[
-                Text("W", style: TextStyle(fontSize: size)),
-                Text("O", style: TextStyle(fontSize: size)),
-                Text("R", style: TextStyle(fontSize: size)),
-                Text("L", style: TextStyle(fontSize: size)),
-                Text("D", style: TextStyle(fontSize: size)),],
+                FlutterLogo(size: 75,), //預設25
+//                Expanded(child: Text("Ww", style: TextStyle(fontSize: size, backgroundColor: Colors.red))), //這樣也無法填滿= =
+                Text("Ww", style: TextStyle(fontSize: size, backgroundColor: Colors.red)),
+                Text("Oo", style: TextStyle(fontSize: size, backgroundColor: Colors.yellow)),
+                Text("Rr", style: TextStyle(fontSize: size, backgroundColor: Colors.green)),
+                Text("Ll", style: TextStyle(fontSize: size, backgroundColor: Colors.blue)),
+                Text("Dd", style: TextStyle(fontSize: size, backgroundColor: Colors.purple)),],
               mainAxisAlignment: MainAxisAlignment.center,
-            )))
+              crossAxisAlignment: CrossAxisAlignment.stretch, //雖然是填滿, 但卻填不滿= =
+            ),
+          ))
     );
   }
 }

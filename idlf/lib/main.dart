bf8019d62719a6747b297658f61b5d9aafa883ae
angelmark <mark.hsieh@angelsctek.com>
2020-07-13 17:26:19 +0800

    field

--- idlf/lib/main.dart
+++ idlf/lib/main.dart
@@ -7,34 +7,14 @@ void main() {
 class MarkApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double size = 100;
 
-    var widgets = <Widget>[
-      FlutterLogo(
-        size: 75,
-      ),
-      //預設25
-//    Expanded(child: Text("Ww", style: TextStyle(fontSize: size, backgroundColor: Colors.red))), //這樣也無法填滿= =
-      Text("Ww", style: TextStyle(fontSize: size, backgroundColor: Colors.red)),
-      Text("Oo",
-          style: TextStyle(fontSize: size, backgroundColor: Colors.yellow)),
-      Text("Rr",
-          style: TextStyle(fontSize: size, backgroundColor: Colors.green)),
-      Text("Ll",
-          style: TextStyle(fontSize: size, backgroundColor: Colors.blue)),
-      Text("Dd",
-          style: TextStyle(fontSize: size, backgroundColor: Colors.purple)),
-    ];
+    TextEditingController textEditingController = new TextEditingController();
 
     void pressed() {
-      print("按！");
+      print(textEditingController.text);
     }
 
     var buttons = <Widget>[
-//      RaisedButton(onPressed: pressed, child: Text("Raised")),
-//      FlatButton(onPressed: pressed, child: Text("Flat")),
-//      OutlineButton(onPressed: pressed, child: Text("Outline")),
-//      IconButton(onPressed: null, icon: ,)),
       Image.asset(
         "resource/images/market.png",
         width: 50,
@@ -45,7 +25,9 @@ class MarkApp extends StatelessWidget {
           "https://img.alicdn.com/imgextra/i4/1033561790/TB2PfEQg17JL1JjSZFKXXc4KXXa_%21%211033561790.jpg"),
       FadeInImage(
           image: NetworkImage("https://img.alicdn.com/imgextra/i4/1033"),
-          placeholder: AssetImage("resource/images/market.png"))
+          placeholder: AssetImage("resource/images/market.png")),
+      IconButton(onPressed: pressed, icon: Icon(Icons.favorite)),
+      TextField(controller: textEditingController, decoration: InputDecoration(hintText: "你有什麼秘密偷偷跟我說我不會告訴別人"),)
     ];
 
     return new MaterialApp(

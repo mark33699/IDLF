f3c05fa1292729c29cf8822b88f0e26a33a66521
angelmark <mark.hsieh@angelsctek.com>
2020-07-15 18:14:27 +0800

    alert

--- idlf/lib/main.dart
+++ idlf/lib/main.dart
@@ -27,9 +27,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   var result = "";
+  String yearOld = "";
 
   @override
   Widget build(BuildContext context) {
+
+//    String yearOld = ""; //放這邊是不會刷新的喔
 
     TextEditingController textEditingController = new TextEditingController();
 
@@ -43,15 +46,56 @@ class _HomePageState extends State<HomePage> {
       setState((){});
     }
 
+    Future<String> showAlert(BuildContext content) async {
+      String answer = "";
+
+      return showDialog(
+          context: context,
+          builder: (BuildContext content) {
+            return AlertDialog(
+              title: Text("彈窗"),
+              content: Row( //用Center會有上下空白, 用Column會下方空很大一塊
+                children: <Widget>[
+                  Expanded(child: TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        labelText: "請問今年貴庚",
+                        hintText: "輸入框就是要輸入啊不然要幹嘛"
+                    ),
+                    onChanged: (value) {
+                      answer = value;
+                    },
+                  ))
+                ],
+              ),
+              actions: <Widget>[
+                RaisedButton(
+                    child: Text("好"),
+                    onPressed: () {
+                      Navigator.pop(context, answer);
+                    }),
+                RaisedButton(
+                    child: Text("才不告訴你～勒～"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+              ],
+            );
+          });
+    }
+
     var widgets = <Widget>[
-      TextField(controller: textEditingController),
+//      TextField(controller: textEditingController),
       OutlineButton(
           child: Text("要去了"),
-          onPressed: () {
-            pushNextPage(context);
+          onPressed: () async {
+//            pushNextPage(context);
+             yearOld = await showAlert(context);
+             setState(() {});
           }
       ),
-      Text("回禮是 $result")
+//      Text("回禮是 $result")
+      Text("您是$yearOld歲")
     ];
 
     return Container(

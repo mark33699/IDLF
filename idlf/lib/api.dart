7cc6673d602b41bf6e92d9ae86d4657109d5e235
angelmark <mark.hsieh@angelsctek.com>
2020-07-20 10:52:01 +0800

    api POST json

--- idlf/lib/api.dart
+++ idlf/lib/api.dart
@@ -29,4 +29,46 @@ class APIManager {
       print("Http Fail is $exp");
     }
   }
+
+  void purchase() async {
+
+    var url = "$domain/api/market/purchase";
+
+    try {
+
+      var map = {
+        "token":"xxxxx",
+        "products":[
+          {
+            "product_id":20
+          },
+          {
+            "product_id":3
+          }
+        ]
+      };
+
+      var request = await HttpClient().postUrl(Uri.parse(url));
+      request.headers.set('content-type', 'application/json');
+      request.add(utf8.encode(json.encode(map)));
+      var response = await request.close();
+
+      if (response.statusCode == HttpStatus.ok) {
+        var json = await response.transform(utf8.decoder).join();
+        var angelMap = jsonDecode(json);
+        print("購買結果$angelMap");
+
+      } else {
+        print("Http NG");
+      }
+    } catch (exp) {
+      print("Http Fail is $exp");
+    }
+  }
+
+  void login(String token) async {
+
+    var url = "$domain/api/login";
+
+  }
 }
--- idlf/lib/page/home_page.dart
+++ idlf/lib/page/home_page.dart
@@ -36,7 +36,9 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     getCount();
     print("初始$yearOld");
-    APIManager().getStore();
+//    APIManager().getStore();
+//    APIManager().login("xxxxx");
+    APIManager().purchase();
   }
 
   @override

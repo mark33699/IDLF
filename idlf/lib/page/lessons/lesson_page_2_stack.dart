8fc09668d60daf3d1f214f79f263255f65f1c6c9
angelmark <mark.hsieh@angelsctek.com>
2020-08-13 18:12:51 +0800

    stack ("Center" not working)

--- idlf/lib/page/lessons/lesson_page_2_stack.dart
+++ idlf/lib/page/lessons/lesson_page_2_stack.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:idlf/define.dart';
 
 class LessonPageStack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Text("Stack"),
+    return Center(
+      child: Stack(
+        children: List.generate(rainbowColors.length, (index) =>
+          Positioned(
+              left: 20 * index.toDouble(),
+              top: 20 * index.toDouble(),
+              child: Container(
+                color: rainbowColors[index],
+                child: null,
+                width: 100,
+                height: 100,
+              )
+          )
+        )
+      )
     );
   }
 }

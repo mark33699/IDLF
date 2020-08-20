9fc900e12ba0f315c6a7598a5da902ec9bbef22a
mark <mark.appledeveloper@gmail.com>
2020-08-21 01:23:02 +0800

    實現 邊框/圓角/陰影

--- idlf/lib/page/lessons/lesson_page_3_decorated.dart
+++ idlf/lib/page/lessons/lesson_page_3_decorated.dart
@@ -2,21 +2,44 @@ import 'package:flutter/material.dart';
 
 class LessonPageDecoratedBox extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.green,
+        child: Center(child: LessonDecoratedBox())
+
+        //終於方咖
+//        Center(child: Container(color: Colors.brown, child: SizedBox(height: 100, width: 100, child: label)))
+        /*滿咖區
+        Container(color: Colors.brown, alignment: Alignment.center, child: SizedBox(height: 100, width: 100, child: label))
+        Container(color: Colors.brown, alignment: Alignment.center, height: 100, width: 100, child: label)
+        Container(color: Colors.brown, child: Center(child: SizedBox(height: 100, width: 100, child: label)))
+        DecoratedBox(decoration: BoxDecoration(color: Colors.brown), child: Center(child: SizedBox(height: 100, width: 100, child: label)))
+        */
+    );
+  }
+}
+
+class LessonDecoratedBox extends StatelessWidget {
+
 //  final label = Text("123\n456\n789", textAlign: TextAlign.center);
   final label = Text("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", textAlign: TextAlign.center);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.green,
-        child:
-
-        Center(child: Container(color: Colors.brown, child: SizedBox(height: 100, width: 100, child: label)))
-//        Container(color: Colors.brown, alignment: Alignment.center, child: SizedBox(height: 100, width: 100, child: label))
-//        Container(color: Colors.brown, alignment: Alignment.center, height: 100, width: 100, child: label)
-//        Container(color: Colors.brown, child: Center(child: SizedBox(height: 100, width: 100, child: label)))
-//        DecoratedBox(decoration: BoxDecoration(color: Colors.brown), child: Center(child: SizedBox(height: 100, width: 100, child: label)))
+    return DecoratedBox(
+//      position: DecorationPosition.foreground, //裝飾層是墊在widget後, 還是檔在widget前, 預設為背景
+      decoration: BoxDecoration(
+        color: Colors.brown,
+        border: Border.all(color: Colors.red, width: 3),
+        borderRadius: BorderRadius.circular(25), //切背景
+        boxShadow: [BoxShadow(offset: Offset(3.0,3.0), blurRadius: 6.0)] //為何要指定多組陰影？
+      ),
+      child: ClipRRect( //切前景
+        clipBehavior: Clip.antiAlias,
+        borderRadius: BorderRadius.circular(50),
+        child: SizedBox(height: 100, width: 100, child: label)
+      )
     );
   }
 }
-

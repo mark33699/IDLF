6c6739f11048dea6d16304d7d356a5876b3c3fd2
angelmark <mark.hsieh@angelsctek.com>
2020-08-28 10:47:56 +0800

    field

--- idlf/lib/page/lessons/lesson_page_4.dart
+++ idlf/lib/page/lessons/lesson_page_4.dart
@@ -1,8 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LessonPage4 extends StatelessWidget {
+
+  //宣告成變數好像就不能hot reload了...
+  final normalText = Text("我就是一個樸實無華的text");
+  final specialText = Text("我是一個置中/粗體/20 size/紅字的text",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.red)
+  );
+
+  final pwdFocus = FocusNode();
+  final mobileField = TextField(
+      controller: null,
+      keyboardType: TextInputType.number,
+      autofocus: true,
+      decoration: InputDecoration(
+          icon: Icon(Icons.account_circle),
+          hintText: "手機號碼"
+      ),
+      onChanged: (string) {
+        print("手機是 $string");
+      },
+  );
+  TextField get pwdField => TextField( //寫成getter才能取另外一個屬性
+      controller: null,
+      obscureText: true,
+      focusNode: pwdFocus,
+      decoration: InputDecoration(
+          icon: Icon(Icons.vpn_key),
+          hintText: "密碼"
+      ),
+      onChanged: (string) {
+        print("密碼是 $string");
+      }
+  );
+
   @override
   Widget build(BuildContext context) {
+
+    pwdFocus.addListener(() {
+      print( "${pwdFocus.hasFocus ? "密碼開始輸入" : "密碼輸入完畢"}" );
+    });
+
     return Scaffold(
         appBar: AppBar(
           title: Text("第四堂課"),
@@ -13,17 +55,25 @@ class LessonPage4 extends StatelessWidget {
           children: <Widget>[
             Container(
               color: Colors.greenAccent,
-              child: Text("我就是一個樸實無華的text")
+              child: normalText
             ),
             Container(
               color: Colors.blueAccent,
               alignment: Alignment.center,
-              child: Text("我是一個置中/粗體/20 size/紅字的text",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red)),
-            )
+              child: specialText
+            ),
+            Container(
+              color: Colors.greenAccent,
+              alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: mobileField
+            ),
+            Container(
+                color: Colors.blueAccent,
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: pwdField
+            ),
           ],
         )
     );

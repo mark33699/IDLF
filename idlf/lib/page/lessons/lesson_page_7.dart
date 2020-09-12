908415f3af100da33f82ccac1ce4dfeeac5d60c1
mark <markHsieH@com.com>
2020-09-13 01:56:19 +0800

    second selector

--- idlf/lib/page/lessons/lesson_page_7.dart
+++ idlf/lib/page/lessons/lesson_page_7.dart
@@ -8,20 +8,60 @@ class LessonPage7 extends StatefulWidget {
 
 class _LessonPage7State extends State<LessonPage7> {
 
-  double bottomWidgetHeight = 200;
+  double bottomWidgetHeight = 0.01;
   String choosed = "你選擇的是";
   Widget selector;
 
-//  Widget datePicker = CupertinoDatePicker(
-//      onDateTimeChanged: (date) {
-//        setState(() { //“Only static members can be accessed in initializers”
-//          print("datepicker");
-//        });
-//      }
-//  );
-
   @override
   Widget build(BuildContext context) {
+
+    Widget datePicker = CupertinoDatePicker(
+      onDateTimeChanged: (date) {
+        setState(() {
+          choosed = "$date";
+        });
+      }
+    );
+
+    Widget actionSheet = CupertinoActionSheet(
+      message: Text("選一個你喜歡的圖案吧"),
+      cancelButton: CupertinoActionSheetAction(
+        child: Text("都不喜歡"),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      actions: [
+        CupertinoActionSheetAction(
+          child: Icon(Icons.all_inclusive),
+          onPressed: () {
+            Navigator.pop(context);
+            setState(() {
+              choosed = "無限";
+            });
+          },
+        ),
+        CupertinoActionSheetAction(
+          child: Icon(Icons.ac_unit),
+          onPressed: () {
+            Navigator.pop(context);
+            setState(() {
+              choosed = "冰雪";
+            });
+          },
+        ),
+        CupertinoActionSheetAction(
+          child: Icon(Icons.help_outline),
+          onPressed: () {
+            Navigator.pop(context);
+            setState(() {
+              choosed = "疑問";
+            });
+          },
+        )
+      ],
+    );
+
     return Scaffold(
         appBar: AppBar(
           title: Text("第七堂課"),
@@ -39,33 +79,39 @@ class _LessonPage7State extends State<LessonPage7> {
                             onPressed: (){
                               setState(() {
                                 bottomWidgetHeight = (bottomWidgetHeight == 200) ? 0.01 : 200;
-                                selector = CupertinoDatePicker(
-                                  onDateTimeChanged: (date) {
-                                    setState(() {
-                                      choosed = "$date";
-                                    });
-                                  }
-                                );
+                                selector = datePicker;
+                                if (bottomWidgetHeight == 0.01) {
+                                  choosed = "你選擇的是";
+                                }
                               });
                             }),
                         ),
                         Align(
                           alignment: Alignment.topRight,
                           child: RaisedButton(
-                              child: Text("Android TimePicker"),
-                              onPressed: (){}),
+                            child: Text("Android TimePicker"),
+                            onPressed: (){
+
+                            }),
                         ),
                         Align(
                           alignment: Alignment.bottomLeft,
                           child: RaisedButton(
-                              child: Text("ActionSheet"),
-                              onPressed: (){}),
+                            child: Text("ActionSheet"),
+                            onPressed: (){
+                              showCupertinoModalPopup(
+                                context: context,
+                                builder: (BuildContext context) => actionSheet
+                              );
+                            }),
                         ),
                         Align(
                           alignment: Alignment.bottomRight,
                           child: RaisedButton(
-                              child: Text("BottomSheet"),
-                              onPressed: (){}),
+                            child: Text("BottomSheet"),
+                            onPressed: (){
+
+                            }),
                         ),
                         Align(
                             alignment: Alignment.center,

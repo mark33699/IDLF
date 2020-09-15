c78f7a683085dc2cce8a0de5f0f92a18073ca0ca
angelmark <mark.hsieh@angelsctek.com>
2020-09-15 11:39:00 +0800

    他媽的用bottomSheet屬性反而害我body被擋住 我還用來幹嘛

--- idlf/lib/page/lessons/lesson_page_8.dart
+++ idlf/lib/page/lessons/lesson_page_8.dart
@@ -7,60 +7,67 @@ class LessonPage8 extends StatefulWidget {
 
 class _LessonPage8State extends State<LessonPage8> {
 
+  final listViewController = ScrollController();
+
   var messages = [//1,2,3];
     "歷史訊息",
     "歷史不能被遺忘",
     "歷史\n歷史\n歷史，\n很重要、說三次",
-    """
-    1
-    2
-    3
-    4
-    5
-    6
-    7
-    8
-    我
-    叫
-    你
-    爸
-    、
-    你
-    打
-    我
-    媽
-    、
-    這
-    樣對嗎？
-    """,
+    """1
+2
+3
+4
+5
+6
+7
+8
+我
+叫
+你
+爸
+、
+你
+打
+我
+媽
+、
+這
+樣對嗎？""",
     "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤😂",
     "總算超過一頁了吧"
   ];
 
   @override
   Widget build(BuildContext context) {
+
+    final list = ListView.builder(
+        controller: listViewController,
+        itemCount: messages.length,
+        itemBuilder: (ctx, idx) {
+          return MessageTile("${messages[idx]}");
+        }
+    );
+
     return Scaffold(
         appBar: AppBar(
           title: Text("第八堂課"),
         ),
-        body:ListView.builder(
-          itemCount: messages.length,
-          itemBuilder: (ctx, idx) {
-            return MessageTile("${messages[idx]}");
-            //return Image.asset("resource/images/bubble_outlined.png");
-          }
-        ),
-        bottomSheet: TextField(
-          decoration: InputDecoration(
-            labelText: '有什麼話想說？',
-            prefixIcon: Icon(Icons.message),
-          ),
-          onSubmitted: (text) {
-            setState(() {
-              messages.add(text);
-            });
-          },
-        ),
+        body: Column(children: <Widget>[
+          Expanded(child: list),
+          TextField(
+            decoration: InputDecoration(
+              labelText: '有什麼話想說？',
+              prefixIcon: Icon(Icons.message),
+            ),
+            onSubmitted: (text) {
+              setState(() {
+                messages.add(text);
+                listViewController.jumpTo(listViewController.position.maxScrollExtent);
+              });
+            },
+          )
+        ]
+      )
     );
   }
 }
@@ -72,10 +79,14 @@ class MessageTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //return Image.asset("resource/images/bubble_outlined.png");
     return Container(
       padding: EdgeInsets.all(8),
-      child: Text(message,
-        textAlign: TextAlign.right //應該是以最長的那句為準置左
+      alignment: Alignment.centerRight,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 200), //如何得到父組件的寬度？
+        child: Text(message),
       )
     );
   }

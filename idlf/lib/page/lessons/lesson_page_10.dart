8b8435d1811b8604ddf46e95b1c55621c8d0b36e
angelmark <mark.hsieh@angelsctek.com>
2020-09-17 14:56:53 +0800

    hud before API

--- idlf/lib/page/lessons/lesson_page_10.dart
+++ idlf/lib/page/lessons/lesson_page_10.dart
@@ -24,17 +24,37 @@ class _LessonPage10State extends State<LessonPage10> {
   @override
   Widget build(BuildContext context) {
 
+    var hud = Container(
+      alignment: Alignment.center,
+      child: CircularProgressIndicator(),
+    );
+
+    var list = ListView.builder(
+        itemExtent: 80,
+        itemCount: (currentUsers == null) ? 0 : currentUsers.userList.length,
+        itemBuilder: (ctx, idx) {
+
+          var user = currentUsers.userList[idx];
+
+          return Card(
+              child: ListTile(
+                leading: Container(
+                  constraints: BoxConstraints(minWidth: 60, minHeight: 60),
+                  color: Colors.black38,
+                  child: Image.network(user.avatar, fit: BoxFit.fill),
+                ),
+                title: Text("${user.firstName} ${user.lastName}"),
+                subtitle: Text(user.email),
+              )
+          );
+        }
+    );
+
     return Scaffold(
         appBar: AppBar(
           title: Text("第十堂課"),
         ),
-        body: ListView.builder(
-          itemExtent: 100,
-          itemCount: (currentUsers == null) ? 0 : currentUsers.userList.length,
-          itemBuilder: (ctx, idx) {
-            return ListTile(title: Text(currentUsers.userList[idx].firstName));
-          }
-        )
+        body: (currentUsers == null) ? hud : list
     );
   }
 }

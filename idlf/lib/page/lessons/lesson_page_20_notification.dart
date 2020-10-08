571d0341e04232daf4b36b93adfc907839db9416
angelmark <mark.hsieh@angelsctek.com>
2020-10-08 13:32:06 +0800

    他媽的誰知道true是不繼續通知？

--- idlf/lib/page/lessons/lesson_page_20_notification.dart
+++ idlf/lib/page/lessons/lesson_page_20_notification.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:idlf/define.dart';
 
-final topLevel = 7;
+final maxLevel = 7;
 
 class LessonPageNotification extends StatefulWidget {
   @override
@@ -15,7 +15,7 @@ class _LessonPageNotificationState extends State<LessonPageNotification> {
         appBar: AppBar(
           title: Text("Notification"),
         ),
-        body: PyramidWidget(topLevel)
+        body: PyramidWidget(1)
     );
   }
 }
@@ -31,20 +31,27 @@ class PyramidWidget extends StatefulWidget {
 
 class _PyramidWidgetState extends State<PyramidWidget> {
 
+  final fireSecond = 1;
   final cubeLong = 25.0;
   bool showLevel = false;
+
+  void fireAfter(int sec) {
+    Future.delayed(Duration(seconds: sec)).then((value) {
+      print("go");
+      PyramidNotification(true).dispatch(context);
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     print(widget.level);
 
-    if (widget.level == 1) {
+    if (widget.level == maxLevel) {
+      showLevel = true;
       print("me");
-      Future.delayed(Duration(seconds: 2)).then((value) {
-        print("go");
-        PyramidNotification(true).dispatch(context);
-      });
+
+      fireAfter(fireSecond);
     }
   }
 
@@ -60,29 +67,32 @@ class _PyramidWidgetState extends State<PyramidWidget> {
 
     final pyramid = Container(
       width: cubeLong * (widget.level * 2 -1),
-      height: cubeLong * (topLevel + 1 - widget.level),
+      height: cubeLong * (maxLevel + 1 - widget.level),
       color: rainbowColors[widget.level - 1],
-      child: NotificationListener<PyramidNotification>(
+      child: showLevel ? levelText : Container(),
+    );
+
+    final stack = Container(color: Color.fromRGBO(0, 0, 0, 0),
+      child: Stack(alignment: Alignment.bottomCenter,
+          children: [
+            pyramid,
+            widget.level == maxLevel ? Container() : PyramidWidget(widget.level + 1), //容器的部分
+          ]
+      ),
+    );
+
+    return NotificationListener<PyramidNotification>(
         onNotification: (notifi){
           print("come");
           if (notifi.ok) {
+            fireAfter(fireSecond);
             setState(() {
               showLevel = true;
             });
           }
-          return true; //繼續向上傳遞
+          return true; //是否到此為止, true: Stop; false: Bubbling
         },
-        child: showLevel ? levelText : Container(),
-      ),
-    );
-
-    return Container(color: Color.fromRGBO(0, 0, 0, 0),
-        child: Stack(alignment: Alignment.bottomCenter,
-          children: [
-            widget.level == 1 ? Container() : PyramidWidget(widget.level - 1), //容器的部分
-            pyramid,
-          ]
-        ),
+        child: stack
     );
   }
 }

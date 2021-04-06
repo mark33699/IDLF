b2cf3023fd2dcf7c4d2a8e03fb3a80ba0af73582
angelmark <mark.hsieh@angelsctek.com>
2021-04-06 14:59:03 +0800

    indent

--- idlf/lib/page/lessons/lesson_page_1_routing.dart
+++ idlf/lib/page/lessons/lesson_page_1_routing.dart
@@ -22,36 +22,36 @@ class LessonPageRouting extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Wrap(
-              children: [
-                Text("$showWording "),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 16.0),
-                        child: TextField(
-                          controller: _editingController,
-                          decoration: InputDecoration(
-                            hintText: "請輸入想跟下一位說的話"
-                          ),
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Wrap(
+            children: [
+              Text("$showWording "),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: TextField(
+                        controller: _editingController,
+                        decoration: InputDecoration(
+                          hintText: "請輸入想跟下一位說的話"
                         ),
                       ),
                     ),
+                  ),
 
-                    OutlineButton(
-                      child: Text("去吧"),
-                      onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => LessonPageRouting(showWording: _editingController.text)
-                        ));
-                      },
-                    )
-                  ],
-                ),
-              ],
-            )
+                  OutlineButton(
+                    child: Text("去吧"),
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => LessonPageRouting(showWording: _editingController.text)
+                      ));
+                    },
+                  )
+                ],
+              ),
+            ],
+          )
         ),
       ),
     );
